83ace8a62d529c2a8ed88925b318b40e57d64279
xushengfeng <xushengfeng_zg@163.com>
2024-05-13 20:34:17 +0800

    1.0.0

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+export class tLog {
+    private tl: { t: string; n: number }[] = [];
+    private name: string;
+    constructor(taskName?: string) {
+        this.name = taskName || "default";
+    }
+    l(name: string, time?: number) {
+        let now = 0;
+        if (time) {
+            now = time - (new Date().getTime() - performance.now());
+        } else {
+            now = performance.now();
+        }
+        this.tl.push({ t: name, n: now });
+        let l: { d: number; n: string; c: number }[] = [];
+        for (let i = 1; i < this.tl.length; i++) {
+            const d = this.tl[i].n - this.tl[i - 1].n;
+            const name = this.tl[i - 1].t;
+            let f = l.find((x) => x.n === name);
+            if (f) {
+                f.c++;
+                f.d += d;
+            } else l.push({ d: d, n: name, c: 1 });
+        }
+        const x: string[] = [];
+        for (let i of l) {
+            const t = i.c > 1 ? `${i.n}x${i.c}` : i.n;
+            x.push(`${t} ${i.d}`);
+        }
+        x.push(this.tl.at(-1).t);
+        console.log(`${this.name} ${l.map((i) => i.d).reduce((p, c) => p + c, 0)}ms: `, x.join(" "));
+    }
+}
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    // 打包配置
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/main.ts"),
+            name: "t_log",
+        },
+    },
+});
